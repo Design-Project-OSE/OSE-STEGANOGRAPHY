99853438411fe322a2db3d0a4662b0fe15523847
SinanUygun <sinan_uygun@windowslive.com>
2023-11-01 18:26:10 +0300

    resim gizleme sorunsuz

--- Gui.py
+++ Gui.py
@@ -1,12 +1,14 @@
 import customtkinter as ctg
-import CTkMessagebox as ctmb
+from tkinter import messagebox
 from PIL import Image
 from Project_Tools import * 
 
 
-from PIL import Image
+
 info=Get_Info()
 tl_file=File_Tool()
+cry=Cryptograph()
+stg=Steganografy()
 class App(ctg.CTk):
     def __init__(self):
         super().__init__()
@@ -23,7 +25,6 @@ class App(ctg.CTk):
         self.title("Project OSE")
         self.iconbitmap("Project_Images\\logo.ico")
         self.geometry(f"{self.widht_size}x{self.height_size}")
-        print(f"{self.widht_size}x{self.height_size}")
 
 
         # Frame Configure
@@ -56,6 +57,7 @@ class MyTabView(ctg.CTkTabview):
         self.loc_save="Project_Images\\save.png"
         self.loc_stopvoice="Project_Images\\stop_voice.png"
         self.loc_voice="Project_Images\\voice.png"
+        self.loc_saveimage="Project_Images\\save_image.png"
 
 
         #Sekme Ekleme
@@ -63,10 +65,11 @@ class MyTabView(ctg.CTkTabview):
         self.add(self.name_tab_voice)
 
         #Button resimleri butim= Button İmage
+        self.butim_saveimage=ctg.CTkImage(Image.open(self.loc_saveimage),size=(50,50))
         self.butim_addimage=ctg.CTkImage(Image.open(self.loc_addimage),size=(50,50))
         self.butim_addvoice=ctg.CTkImage(Image.open(self.loc_addvoice),size=(50,50))
         self.butim_download=ctg.CTkImage(Image.open(self.loc_download),size=(50,50))
-        self.butim_imageframe=ctg.CTkImage(Image.open(self.loc_imageframe),size=((3*(self.widht_size/16)-20),self.height_size/2-40))
+        self.butim_imageframe=ctg.CTkImage(Image.open(self.loc_imageframe),size=((4*(self.widht_size/16)-40),self.height_size/2-60))
         self.butim_playvoice=ctg.CTkImage(Image.open(self.loc_playvoice),size=(50,50))
         self.butim_save=ctg.CTkImage(Image.open(self.loc_save),size=(50,50))
         self.butim_stopvoice=ctg.CTkImage(Image.open(self.loc_stopvoice),size=(50,50))
@@ -76,45 +79,175 @@ class MyTabView(ctg.CTkTabview):
         #RESİM İÇİNE METİN GİZLEME ALANI İÇİN 
         #frameler
 
-        self.frame_tabimage=ctg.CTkFrame(master=self.tab(self.name_tab_image),width=(3*(self.widht_size/16)),height=self.height_size/2-20)
+        self.frame_tabimage=ctg.CTkFrame(master=self.tab(self.name_tab_image),width=(4*(self.widht_size/16)),height=self.height_size/2-40)
         self.frame_tabimage.grid(row=1,column=0,padx=10,pady=10)
 
-        self.frame_button_tabimage=ctg.CTkFrame(master=self.tab(self.name_tab_image),width=120,height=self.height_size/2-20)
-        self.frame_button_tabimage.grid(row=1,column=1,padx=10,pady=10)
+        self.frame_button_tabimage=ctg.CTkFrame(master=self.tab(self.name_tab_image),width=(4*(self.widht_size/16)),height=70)
+        self.frame_button_tabimage.grid(row=2,column=0,padx=5,pady=5)
 
-        self.frame_tabtextbox=ctg.CTkFrame(master=self.tab(self.name_tab_image),width=(3*(self.widht_size/16)),height=self.height_size/2-20)
-        self.frame_tabtextbox.grid(row=1,column=2,padx=10,pady=10)
+        self.frame_tabtextbox=ctg.CTkFrame(master=self.tab(self.name_tab_image),width=(4*(self.widht_size/16)),height=self.height_size/2-40)
+        self.frame_tabtextbox.grid(row=1,column=1,padx=5,pady=5)
 
-        self.frame_button_tabtextbox=ctg.CTkFrame(master=self.tab(self.name_tab_image),width=120,height=self.height_size/2-20)
-        self.frame_button_tabtextbox.grid(row=1,column=3,padx=10,pady=10)
+        self.frame_button_tabtextbox=ctg.CTkFrame(master=self.tab(self.name_tab_image),width=(4*(self.widht_size/16)),height=70)
+        self.frame_button_tabtextbox.grid(row=2,column=1,padx=5,pady=5)
 
+        self.frame_text_key=ctg.CTkFrame(master=self.frame_tabtextbox,width=(4*(self.widht_size/16))-20,height=70)
+        self.frame_text_key.grid(row=1,column=0,padx=5,pady=5)
+
+        #Textbox 
+        self.entry=ctg.CTkTextbox(master=self.frame_tabtextbox,width=(4*(self.widht_size/16))-20,height=(self.height_size/2-120),text_color="#A5D7E8",font=("Open Sans",16))
+        self.entry.grid(row=0,column=0,padx=5,pady=5)
+
+        #Entry 
+        self.entry_key=ctg.CTkEntry(master=self.frame_text_key,placeholder_text="key",width=((4*(self.widht_size/16))-20)/2,height=50,text_color="#A5D7E8",font=("Open Sans",16))
+        self.entry_key.grid(row=0,column=0,padx=5,pady=5)
+
+    
+
+        #Resim Alanı
+        self.ilabel_mainimage=ctg.CTkLabel(master=self.frame_tabimage,
+        width=(4*(self.widht_size/16))-20,
+        height=self.height_size/2-60,
+        text="",
+        image=self.butim_imageframe)
+        self.ilabel_mainimage.grid(row=0,column=0,padx=10,pady=10) 
+
+        #fonksiyonlar
+        def image_configure(image_pil):
+            self.config_image=ctg.CTkImage(image_pil,size=((4*(self.widht_size/16)-40),self.height_size/2-60))
+            return self.config_image
+
+            
 
         #Eventler
         def event_button_open_image():
             self.loc_file_image=tl_file.open_file()
-            self.open_image=ctg.CTkImage(Image.open(self.loc_file_image),size=(((3*(self.widht_size/16)-40),self.height_size/2-60)))
-            self.ilabel_frimage.configure(image=self.open_image)
+            self.image_tool=Image.open(self.loc_file_image)
+            self.open_image=ctg.CTkImage(Image.open(self.loc_file_image),size=((4*(self.widht_size/16)-40),(self.height_size/2-60)))
+            self.ilabel_mainimage.configure(image=self.open_image)
 
+
+
+        def event_button_clear_text():
+            self.entry.delete("0.0","end")
+
+        def event_button_encrypt():
+            self.key=cry.get_key()
+            self.msg_byt=cry.transform_bytes(self.entry.get("0.0","end"))
+            self.msg_enc=cry.encrypt_msg(self.msg_byt,self.key)
+            self.entry.delete("0.0","end")
+            self.entry_key.delete(0,"end")
+            self.entry_key.insert(0,self.key.decode('utf-8'))
+            self.entry.insert("end",f"{self.msg_enc.decode('utf-8')}")
+
+        def event_button_hide_text():
+            self.key=cry.get_key()
+            self.msg_byt=cry.transform_bytes(self.entry.get("0.0","end"))
+            self.msg_enc=cry.encrypt_msg(self.msg_byt,self.key)
+            self.entry.delete("0.0","end")
+            self.entry_key.delete(0,"end")
+            self.entry_key.insert(0,self.key.decode('utf-8'))
+            self.entry.insert("end",f"{self.msg_enc.decode('utf-8')}")
+            self.image_hide=stg.hideData(self.loc_file_image,self.entry.get("0.0","end"))
+            self.ilabel_mainimage.configure(image=image_configure(self.image_hide))
+
+
+
+        def event_button_show_text():
+            self.show_text=stg.showData(self.loc_file_image)
+            self.entry.delete("0.0","end")
+            self.entry.insert("end",f"{self.show_text}")
+
+        def event_button_unlock_key():
+            self.unlock_key=cry.transform_bytes(self.entry_key.get())
+            self.unlock_text=cry.transform_bytes(self.entry.get("0.0","end"))
+            self.unlock_decrypt=cry.decrypt_msg(self.unlock_text,self.unlock_key)
+            self.entry.delete("0.0","end")
+            self.entry.insert("end",self.unlock_decrypt.decode("utf-8"))
+
+        def event_button_save_image():
+            self.loc_file_image=tl_file.saveData(self.image_hide)
+            print(self.loc_file_image)
         #Buttonlar
         self.button_open_image=ctg.CTkButton(
             master=self.frame_button_tabimage,
             image=self.butim_addimage,
             width=50,
             height=50,
-            text=None,
+            text="Open",
+            text_color="#0174BE",
+            font=("Open Sans",16),
             command=event_button_open_image,
-            fg_color="transparent"
+            fg_color="#181D31"
         )
         self.button_open_image.grid(row=0,column=0,padx=10,pady=10)
         
+        self.button_save_image=ctg.CTkButton(
+            master=self.frame_button_tabimage,
+            image=self.butim_addimage,
+            width=50,
+            height=50,
+            text="Save Image",
+            text_color="#0174BE",
+            font=("Open Sans",16),
+            command=event_button_save_image,
+            fg_color="#181D31"
+        )
+        self.button_save_image.grid(row=0,column=1,padx=10,pady=10)
+        
+        self.button_hide_text=ctg.CTkButton(
+            master=self.frame_button_tabtextbox,
+            image=self.butim_save,
+            width=50,
+            height=50,
+            text="Hide Text",
+            text_color="#0174BE",
+            font=("Open Sans",16),
+            command=event_button_hide_text,
+            fg_color="#181D31",
+        )
+        self.button_hide_text.grid(row=0,column=0,padx=10,pady=10)
 
-        #Resim Alanı
-        self.ilabel_frimage=ctg.CTkLabel(master=self.frame_tabimage,
-        width=(3*(self.widht_size/16))-20,
-        height=self.height_size/2-40,
-        text="",
-        image=self.butim_imageframe)
-        self.ilabel_frimage.grid(row=0,column=0,padx=10,pady=10) 
+        self.button_show_text=ctg.CTkButton(
+            master=self.frame_button_tabtextbox,
+            image=self.butim_stopvoice,
+            width=50,
+            height=50,
+            text="Show Text",
+            text_color="#0174BE",
+            font=("Open Sans",16),
+            command=event_button_show_text,
+            fg_color="#181D31",
+        )
+        self.button_show_text.grid(row=0,column=1,padx=10,pady=10)
+
+        self.button_clear_text=ctg.CTkButton(
+            master=self.frame_button_tabtextbox,
+            image=self.butim_stopvoice,
+            width=50,
+            height=50,
+            text="Clear Text",
+            text_color="#0174BE",
+            font=("Open Sans",16),
+            command=event_button_clear_text,
+            fg_color="#181D31",
+        )
+        self.button_clear_text.grid(row=0,column=2,padx=10,pady=10)
+
+
+        self.button_unlock_key=ctg.CTkButton(
+            master=self.frame_text_key,
+            image=self.butim_stopvoice,
+            width=50,
+            height=50,
+            text="Unlock Text",
+            text_color="#0174BE",
+            font=("Open Sans",16),
+            command=event_button_unlock_key,
+            fg_color="#181D31",
+        )
+        self.button_unlock_key.grid(row=0,column=1,padx=5,pady=5)
+
        
 
 
--- Project_Tools.py
+++ Project_Tools.py
@@ -1,21 +1,38 @@
-from tkinter import filedialog as fd
-import pyautogui
 import os
 import CTkMessagebox as messagebox
+from PIL import Image
 
+import pyautogui
+from tkinter import filedialog as fd
 class File_Tool:
+    count=1
     def open_file(self):
         super().__init__()
         try:
             self.loc_file=fd.askopenfilename(initialdir=os.getcwd(),  
-                                                title='Select Image File', 
+                                                title='Open Image File', 
                                                 filetypes=(("PNG File", "*.png"), 
                                                             ("JPG File", "*.jpg"), 
                                                             ("JPEG Fİle", "*.jpeg"), ("All Files", "*.*")))
+        
 
         except:
             messagebox.showwarning("warning", "You didn't pick an image! Please choose an image")
         return self.loc_file
+
+    def saveData(self,save_file):
+        super().__init__()
+        loc_save=fd.asksaveasfilename(defaultextension=".png",initialdir=os.getcwd(),  
+                                                title='Save Image File', 
+                                                filetypes=(("PNG File", "*.png"), 
+                                                            ("JPG File", "*.jpg"), 
+                                                            ("JPEG Fİle", "*.jpeg"), ("All Files", "*.*")))
+        if loc_save:
+            save_file.save(loc_save)
+
+        return loc_save
+
+
 
 class Get_Info:
         def screen_size(self):
@@ -26,5 +43,37 @@ class Get_Info:
                 messagebox.showwarning("warning","Something went wrong.")
             return self.width,self.height
 
+from stegano import lsb
+class Steganografy():
+
+    def hideData(self,filename,message):
+        super().__init__()
+        return lsb.hide(str(filename), message,encoding="UTF-8",auto_convert_rgb=True) # lsb.hide() fonksiyonu, mesajı resim dosyasına gizler. Mesaj gizlenen resim secret değişkeninde atılır
+
+
+    def showData(self,filename):
+        super().__init__()
+        return lsb.reveal(filename,encoding="UTF-8") # lsb.reveal() fonksiyonu, gizlenmiş mesajı resim dosyasından çıkarır.
+
+        
+from cryptography.fernet import Fernet        
+class Cryptograph():
+    def get_key(self):
+        super().__init__()
+        return Fernet.generate_key()
+        
+    def transform_bytes(self,str):#key ve şifrelenmiş yapının bayt formatında olması lazım
+        super().__init__()
+        return bytes(str,'utf-8')    
+
+    def encrypt_msg(self,msg,key):
+        super().__init__()
+        fernet=Fernet(key)
+        return fernet.encrypt(msg)
+
+    def decrypt_msg(self,msg,key):
+        super().__init__()
+        fernet=Fernet(key)
+        return fernet.decrypt(msg)
 
 
